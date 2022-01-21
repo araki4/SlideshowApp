a879eef3d8834106c2b9786f9bf4d6b6477c8173
ryouta.araki4 <ryouta.araki4@gmail.com>
2022-01-21 16:25:59 +0900

    操作ボタン処理追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -18,18 +18,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     // ダミー用のスライド用のIBOutlet
     @IBOutlet weak var dummyScrollView: UIScrollView!
+    // 次へボタン用のIBOutlet
+    @IBOutlet weak var nextButtonOutlet: UIButton!
+    // 戻るボタン用のIBOutlet
+    @IBOutlet weak var backButtonOutlet: UIButton!
+    // 再生・停止ボタン用のIBOutlet
+    @IBOutlet weak var playStopButtonOutlet: UIButton!
     
     
-    
-    // 仮置きの進むボタン
-    @IBAction func testButton(_ sender: Any) {
+    // 進むボタンのIBAction
+    @IBAction func nextButton(_ sender: Any) {
         slideImage(direction: "right")
-        
+    }
+    // 戻るボタンのIBAction
+    @IBAction func backButton(_ sender: Any) {
+        slideImage(direction: "left")
     }
     
-    // 仮置きの戻るボタン
-    @IBAction func testButton2(_ sender: Any) {
-        slideImage(direction: "left")
+    // 自動スライド設定
+    var autoSlideTimer: Timer? = nil
+    
+    // 再生・停止ボタンのIBAction
+    @IBAction func playStopButton(_ sender: Any) {
+        // 動作中のタイマーを1つに保つために、 timer が存在しない場合だけ、タイマーを生成して動作させる
+        if self.autoSlideTimer == nil {
+            self.autoSlideTimer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(autoSlideImage(_:)), userInfo: nil, repeats: true)
+            self.playStopButtonOutlet.setTitle("停止", for: .normal)
+            self.nextButtonOutlet.isEnabled = false
+            self.backButtonOutlet.isEnabled = false
+        }else {
+            self.autoSlideTimer!.invalidate()
+            self.autoSlideTimer = nil
+            self.playStopButtonOutlet.setTitle("再生", for: .normal)
+            self.nextButtonOutlet.isEnabled = true
+            self.backButtonOutlet.isEnabled = true
+        }
     }
     
     // 画像リストのインデックス
@@ -54,8 +77,10 @@ class ViewController: UIViewController {
             imageIndex = imageIndex == imageList.endIndex - 1 ? 0 : imageIndex + 1
         }
 
+        // ダミースライドの設定
+        // スライド元のX座標を指定
+        // ダミー画像を変更
         self.dummyScrollView.transform = CGAffineTransform(translationX: -(self.translationX), y: 0)
-        // スライド成功時に、次の画像の表示と表示位置を調整
         self.dummyImageView.image = UIImage.init(named: self.imageList[self.imageIndex])
 
         
@@ -63,17 +88,20 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 1, animations: {
             //　スライドの実行
             self.scrollView.transform = CGAffineTransform(translationX: self.translationX, y: 0)
-            //　スライドの実行
+            //　ダミースライドの実行
             self.dummyScrollView.transform = CGAffineTransform(translationX: 0, y: 0)
             
         }, completion: {finished in
             // スライド成功時に、次の画像の表示と表示位置を調整
             self.imageView.image = UIImage.init(named: self.imageList[self.imageIndex])
             self.scrollView.transform =  CGAffineTransform(translationX: 0, y: 0)
-            // UIView.animate(withDuration: 1, animations: {self.scrollView.transform =  CGAffineTransform(translationX: 0, y: 0)})
         })
     }
     
+    //
+    @objc func autoSlideImage(_ timer: Timer){
+        slideImage(direction: "right")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
